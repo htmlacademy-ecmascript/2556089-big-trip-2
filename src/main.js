90cd2ac89fba81e5c383af4dd21b0baf7af7713c
Mikhail Voronov <mono.vmv@gmail.com>
2025-03-14 20:18:37 +0300

    Добавлен модуль с формой сортировки. Переименована чать файлов в view

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-import NewFilterListView from './view/filters.js';
+import FilterListView from './view/filters-view.js';
+import FormEditingView from './view/form-sorting-view.js';
 import {render} from './render.js';
 
 const siteHeader = document.querySelector('.page-header');
-const siteFilterList = siteHeader.querySelector('.trip-controls__filters');
-console.log (siteHeader);
-console.log (siteFilterList);
+const filterList = siteHeader.querySelector('.trip-controls__filters');
+const siteMain = document.querySelector('.page-main');
+const formSortingSection = siteMain.querySelector('.trip-events');
 
-render (new NewFilterListView (), siteFilterList);
+// const tripEventList = siteMain.querySelector('.trip-events');
+// console.log (tripEventList);
+
+render (new FilterListView (), filterList);
+render (new FormEditingView(), formSortingSection);
 
 
