46f9832f15220355ff5a91b023c8349b060495cb
Mikhail Voronov <mono.vmv@gmail.com>
2025-03-15 20:29:09 +0300

    Добавлен view списка событий

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import FilterListView from './view/filters-view.js';
 import FormEditingView from './view/form-sorting-view.js';
+import EventListView from './view/event-list-view.js';
 import {render} from './render.js';
 
 const siteHeader = document.querySelector('.page-header');
@@ -7,10 +8,8 @@ const filterList = siteHeader.querySelector('.trip-controls__filters');
 const siteMain = document.querySelector('.page-main');
 const formSortingSection = siteMain.querySelector('.trip-events');
 
-// const tripEventList = siteMain.querySelector('.trip-events');
-// console.log (tripEventList);
-
 render (new FilterListView (), filterList);
 render (new FormEditingView(), formSortingSection);
+render(new EventListView (), formSortingSection);
 
 
